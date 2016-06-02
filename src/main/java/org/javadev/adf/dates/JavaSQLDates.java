852b597df0d55f445c657831951b4c2cb3fd6f36
plsql <m.a.r.l.e.y@mail.ru>
2016-06-02 19:26:59 +0300

    Dates Dummy Added

---
+++ src/main/java/org/javadev/adf/dates/JavaSQLDates.java
@@ -0,0 +1,15 @@
+package org.javadev.adf.dates;
+
+public class JavaSQLDates {
+    
+    public String TODAY;
+        
+        
+    public void printAllDates() {
+
+        System.out.println("TODAY: " + this.TODAY);
+
+    }
+    
+    
+} // The End of Class;
