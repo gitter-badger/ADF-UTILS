9fd8d42f4845988db16afa73fc8938c28a598ac2
plsql <m.a.r.l.e.y@mail.ru>
2016-06-02 19:34:30 +0300

    upd

---
+++ src/main/java/org/javadev/adf/dates/JavaSQLDatesImpl.java
@@ -0,0 +1,18 @@
+package org.javadev.adf.dates;
+
+public class JavaSQLDatesImpl implements JavaSQLDates {
+            
+            
+    @Override
+    public String getToday() {
+        // TODO Implement this method
+        return null;
+    }
+    
+    
+    @Override
+    public void printAllDates() {
+        System.out.println("Dates");
+    }
+    
+} // The End of Class;
