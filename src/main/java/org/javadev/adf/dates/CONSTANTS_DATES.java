7ab4f7db275e8ff31aa3dc6916fca69b6125b9bf
plsql <m.a.r.l.e.y@mail.ru>
2016-06-28 14:01:44 +0300

    Dates Update Methods

--- src/main/java/org/javadev/adf/dates/CONSTANTS_DATES.java
+++ src/main/java/org/javadev/adf/dates/CONSTANTS_DATES.java
@@ -4,5 +4,7 @@ public class CONSTANTS_DATES {
     
     public static final String OutputDateFormat = "dd-MM-yyyy";
     
+    public static final String THIS_WEEK = "ThisWeek";
+    
     
 } // The End of Class;
--- src/main/java/org/javadev/adf/dates/entity/JavaSQLDatePeriod.java
+++ src/main/java/org/javadev/adf/dates/entity/JavaSQLDatePeriod.java
@@ -2,6 +2,9 @@ package org.javadev.adf.dates.entity;
 
 import java.sql.Date;
 
+import org.javadev.adf.dates.CONSTANTS_DATES;
+import org.javadev.adf.dates.JavaSQLDates;
+
 public class JavaSQLDatePeriod {
     
     
@@ -28,11 +31,37 @@ public class JavaSQLDatePeriod {
     }
     
     // -----------------------------------
+
+    String periodName;
+    
+    public void setPeriodName(String periodName) {
+        this.periodName = periodName;
+    }
+
+    public String getPeriodName() {
+        return periodName;
+    }
+    
+    // -----------------------------------
     
     public JavaSQLDatePeriod(java.sql.Date date1, java.sql.Date date2) {
         this.date1 = date1;
         this.date2 = date2;
     }
 
+    // -----------------------------------
     
+    public JavaSQLDatePeriod(String periodName) {
+        
+        // TODO: All needed period
+        
+        if (periodName.equals(CONSTANTS_DATES.THIS_WEEK)){
+            
+            this.date1 = JavaSQLDates.getFirstDayOfThisWeek();
+            this.date2 = JavaSQLDates.getLastDayOfThisWeek();
+            
+            this.periodName = CONSTANTS_DATES.THIS_WEEK;
+        }
+    }
+
 } // The End of Class;
